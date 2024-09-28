566971972c3da3e88106785d3eb310e74d06d206
vini <viniciuspauloom@hotmail.com>
2024-09-28 00:29:07 -0300

    BONECO

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -1,5 +1,11 @@
 from objects.obj import Boneco
 
-p1 = Boneco("Porra", "Guerreiro", 0, 0, 100, 10)
+p1 = Boneco("Porra", "Guerreiro", 0, 0, 100, 50)
 
 p1.show_atr()
+
+print(p1.dano())
+print(p1.dano())
+print(p1.vida())
+
+
